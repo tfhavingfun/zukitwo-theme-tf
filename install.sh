64e262fb587c76be19cd5fef4bb3939eac011598
Tyler Foo <tftylerfoo@gmail.com>
2019-11-03 17:04:52 +0800

    added install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ~/.themes/zukitwo-theme-tf/gnome-shell ~/.themes/zukitwo-themes-tf/gtk-2.0 ~/.themes/zukitwo-themes-tf/gtk-3.0
+
+cp -rf ./gnome-shell/src/*.svg ./gnome-shell/src/*.css ./gnome-shell/src/*.png ~/.themes/zukitwo-theme-tf/gnome-shell
+
+cp -rf ./gtk/src/Zukitwo/gtk-2.0/* ~/.themes/zukitwo-themes-tf/gtk-2.0
+
+cp -rf ./gtk/src/Zukitwo/gtk-3.0/assets ./gtk/src/Zukitwo/gtk-3.0/*.css ~/.themes/zukitwo-theme-tf/gtk-3.0
+
+cp ./gtk/src/index.theme ~/.themes/zukitwo-theme-tf
+
