7e323fa6c8c707be41d105c14de9d73817418f7e
Tyler Foo <tftylerfoo@gmail.com>
2019-11-03 17:09:09 +0800

    fixed a typo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.themes/zukitwo-theme-tf/gnome-shell ~/.themes/zukitwo-theme-tf/gtk-2
 
 cp -rf ./gnome-shell/src/*.svg ./gnome-shell/src/*.css ./gnome-shell/src/*.png ~/.themes/zukitwo-theme-tf/gnome-shell
 
-cp -rf ./gtk/src/Zukitwo/gtk-2.0/* ~/.themes/zukitwo-themes-tf/gtk-2.0
+cp -rf ./gtk/src/Zukitwo/gtk-2.0/* ~/.themes/zukitwo-theme-tf/gtk-2.0
 
 cp -rf ./gtk/src/Zukitwo/gtk-3.0/assets ./gtk/src/Zukitwo/gtk-3.0/*.css ~/.themes/zukitwo-theme-tf/gtk-3.0
 
